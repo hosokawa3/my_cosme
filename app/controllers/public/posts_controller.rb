482558ca54721c9392cb34f2db73003d76a8f17d
hosokawa3 <clover091039@gmail.com>
2024-04-18 02:50:05 +0000

    [Add]post_new

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,11 @@
 class Public::PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.find(params[:id])
+    post.save
   end
 
   def index
