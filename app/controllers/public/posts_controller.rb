8a48458fb968892eaf2abf35463b704b0d994377
hosokawa3 <clover091039@gmail.com>
2024-05-11 13:09:26 +0000

    [Add]投稿の編集と削除

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -9,7 +9,6 @@ class Public::PostsController < ApplicationController
     if @post.save
       redirect_to posts_path
     else
-      puts @post.errors.full_messages
       render :new
     end
   end
@@ -23,7 +22,24 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to post_path(post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
 
   private
 
