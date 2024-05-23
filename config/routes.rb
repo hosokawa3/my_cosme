51aa3e36031fc58b9b156d36e37a6b8135002abe
hosokawa3 <clover091039@gmail.com>
2024-05-23 03:59:05 +0000

    [Add]ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
   scope module: :public do
     root to: 'homes#top'
+    get 'users/profile/:id' => 'users#show', as: 'profile'
+    get 'users/information/edit/:id' => 'users#edit', as: 'edit_information'
+    patch 'users/information/:id' => 'users#update', as: 'update_information'
     resources :posts, only: [:new, :create, :update, :edit, :show, :index, :destroy]
-    resources :users, only: [:show, :edit, :update]
+    # resources :users, only: [:show, :edit, :update]
   end
   #会員側
   devise_for :users,skip: [:passwords], controllers: {
